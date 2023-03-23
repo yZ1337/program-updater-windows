72491fca380886413003538a7a2b44868d8a87cd
tv33 <thomas.verhoeven3@hva.nl>
2023-03-23 11:10:28 +0100

    Changes

--- run.bat
+++ run.bat
@@ -1,8 +1,9 @@
 @echo off
+title Run/Python checker
 python --version 2>NUL
 if errorlevel 1 goto errorNoPython
-echo Python already installed!
-update.py
+start update.py
+exit
 
 goto :EOF
 
@@ -11,3 +12,4 @@ echo/
 echo Error^: Python not installed
 echo Install python via this link: https://www.python.org/ftp/python/3.11.2/python-3.11.2-amd64.exe
 pause
+
